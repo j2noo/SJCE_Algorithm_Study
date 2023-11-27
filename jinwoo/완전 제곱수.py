b45708d5feac962481d1b1e7feeaf9b2849bdc2c
j2noo <wlsdndml213@naver.com>
2023-11-27 16:24:39 +0900

    [2주차] 완전 제곱수.py

--- jinwoo/완전 제곱수.py
+++ jinwoo/완전 제곱수.py
@@ -1,1 +1,23 @@
-test
+import math
+
+powerArr = []
+for i in range(101):
+    powerArr.append(i*i)
+    
+M=int(input())
+N=int(input())
+
+startIdx = math.ceil(math.sqrt(M))
+endIdx =math.floor(math.sqrt(N))
+
+
+sum=0
+for i in range(startIdx,endIdx+1):
+    sum+=powerArr[i]
+    
+if startIdx>endIdx:
+    print(-1)
+
+else :
+    print(sum)
+    print(powerArr[startIdx])
