ac705bec73f5d49e4c60b3d2c1a41cc168ba7e01
j2noo <wlsdndml213@naver.com>
2023-12-01 03:19:07 +0900

    [2주차]가장 긴 증가하는 부분수열

---
+++ jinwoo/2주차/LIS.py
@@ -0,0 +1,13 @@
+N=int(input())
+
+arr = list(map(int,input().split()))
+
+dp = [1]*N
+ans=1
+
+for i in range(N):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+            ans=max(ans,dp[i])
+print(ans)
