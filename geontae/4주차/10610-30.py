7385b87023c341c2e7be0556156f56adf409668d
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-01-10 22:51:16 +0900

    10610-30

--- geontae/4주차/10610-30.py
+++ geontae/4주차/10610-30.py
@@ -1,15 +1,13 @@
 N = int(input())
-
 arr = []
-cnt = 0
+
 for i in str(N):
-    cnt += int(i)
     arr.append(int(i))
 
 arr.sort(reverse=True)
 result = int(''.join(map(str, arr)))
 
-if cnt % 3 == 0 and result % 10 == 0:
+if result % 30 == 0:
     print(result)
 else:
     print(-1)
