930db6ef0b98178a25c941f1fda85b2080566cae
j2noo <wlsdndml213@gmail.com>
2024-02-26 13:49:32 +0900

    6주차 : 쉬운 계단 수

--- Jiseob/Week 1/팰린드롬인지 확인하기.py
+++
@@ -1,6 +0,0 @@
-userInput = input()
-
-if userInput[::1] == userInput[::-1]:
-    print(1)
-else:
-    print(0)
---
+++ jinwoo/6주차/쉬운 계단 수.py
@@ -0,0 +1,22 @@
+MOD = 1000000000
+
+# n자리, k로 끝나는 계단 수의 개수
+dp = [[-1]*11 for I in range(101)]
+
+# 1자리 계단수
+dp[1][0]=0
+for i in range(1,10):
+  dp[1][i]=1
+  
+for i in range(2,101):
+  dp[i][0] = dp[i-1][1]
+  dp[i][9] = dp[i-1][8]
+  for j in range(1,9):
+    dp[i][j] = dp[i-1][j-1]+dp[i-1][j+1]
+    dp[i][j]%=MOD
+
+n=int(input())
+sum=0
+for i in range(10):
+  sum+=dp[n][i]
+print(sum%MOD)
