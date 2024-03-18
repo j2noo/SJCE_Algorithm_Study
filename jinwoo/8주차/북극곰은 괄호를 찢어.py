6ebefb7693e13eb45a981fc0ddbeb5aa3e330829
j2noo <wlsdndml213@gmail.com>
2024-03-18 22:11:21 +0900

    10주차 업데이트

--- jinwoo/8주차/북극곰은 괄호를 찢어.py
+++ jinwoo/8주차/북극곰은 괄호를 찢어.py
@@ -1,100 +1,36 @@
-# def solve1(str1):
-#     day=0
-#     while(True):
-#         beforeLen = len(str1)
-#         str1=str1.replace(')(','x')
-#         str1=str1.replace("()",'x')
-#         str1=str1.replace('x','')
-#         afterLen = len(str1)
-#         if beforeLen == afterLen :
-#             break
-#         day+=1
+def solve1(str1):
+    day=0
+    while((str1.count(')(')+str1.count('()'))>0):
+        str1=str1.replace(')(','x')
+        str1=str1.replace("()",'x')
+        str1=str1.replace('x','')
+        day+=1
 
-#     if str1!='':
-#         day=987654321
-#     return day
+    if str1!='':
+        day=987654321
+    return day
     
 
-# def solve2(str1):
-#     day=0
-#     while(True):
-#         beforeLen = len(str1)
-#         str1=str1.replace("()",'x')
-#         str1=str1.replace(')(','x')
-#         str1=str1.replace('x','')
-#         afterLen = len(str1)
-#         if beforeLen == afterLen :
-#             break
-#         day+=1
+def solve2(str1):
+    day=0
+    while((str1.count(')(')+str1.count('()'))>0):
+        str1=str1.replace("()",'x')
+        str1=str1.replace(')(','x')
+        str1=str1.replace('x','')
+        day+=1
 
-#     if str1!='':
-#         day=987654321
-#     return day
+    if str1!='':
+        day=987654321
+    return day
 
 N=int(input())
 str= input()
 strCpy = str
 
-day=0
-while(True):
+# call by value?
+ans = min(solve1(str),solve2(strCpy))
+if ans==987654321 :
+    ans=-1
     
-    beforeLen = len(str)
-    prev = 'z'
-    newStr = ''
-    for i in range(len(str)):
-        if prev=='(':
-            if str[i] ==')':
-                prev='x'
-            else :
-                newStr+=prev
-                prev = str[i]
-                
-        elif prev==')':
-            if str[i] =='(':
-                prev='x'
-            else :
-                newStr+=prev
-                prev = str[i]
-        else :
-            prev = str[i]
-            # newStr+=prev  !!
-    afterLen = len(newStr)
-    str= newStr 
-    day+=1
-    if beforeLen == afterLen :
-        break
-    day2=0
-while(True):
-    beforeLen = len(strCpy)
-    prev = 'z'
-    newStr = ''
-    for i in range(len(strCpy)):
-        if prev==')':
-            if strCpy[i] =='(':
-                prev='x'
-            else :
-                newStr+=prev
-                prev = strCpy[i]
-                
-        elif prev=='(':
-            if strCpy[i] ==')':
-                prev='x'
-            else :
-                newStr+=prev
-                prev = strCpy[i]
-        else :
-            prev = strCpy[i]
-            # newStr+=prev  !
-    afterLen = len(newStr)
-    strCpy= newStr 
-    day2+=1
-    if beforeLen == afterLen :
-        break
-   
-print(day,day2,str)    
-
-# if ans==987654321 :
-#     ans=-12
-    
-
+print(ans)
 
