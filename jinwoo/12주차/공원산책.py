ee4709616f0c9f05d12ae8dd0851f2a659db2559
j2noo <wlsdndml213@gmail.com>
2024-07-06 23:48:16 +0900

    12주차 : 공원산책

--- jinwoo/12주차/공원산책.py
+++ jinwoo/12주차/공원산책.py
@@ -1,1 +1,24 @@
-print("SSSs")
+# 18분
+dir = {'N' : [-1,0], 'S' : [1,0],'W' : [0,-1],'E' : [0,1] }
+def solution(park, routes):
+    
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j]=='S':
+                r,c = i,j
+                
+    for route in routes:
+        impossibleFlag=0
+        op,n =route[0],int(route[2])
+        dy = dir[op][0]
+        dx = dir[op][1]
+        nr,nc = r,c
+        for i in range(n):
+            nr,nc = nr + dy, nc + dx
+            if nr<0 or nc<0 or nr>=len(park) or nc >= len(park[0]) or  park[nr][nc]=='X':
+                impossibleFlag=1
+                break
+        if impossibleFlag==0:
+            r,c = nr,nc
+
+    return [r,c]
