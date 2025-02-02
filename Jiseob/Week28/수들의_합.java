5bd0044bb65ba28ad8bf138ee896dfbddbe5d0fc
icebear0111 <wltjq0901@naver.com>
2025-02-02 23:32:24 +0900

    [28주차] 수들의 합

---
+++ Jiseob/Week28/수들의_합.java
@@ -0,0 +1,20 @@
+package Week28;
+
+import java.util.*;
+import java.io.*;
+
+public class 수들의_합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long S = Long.parseLong(br.readLine());
+
+        long n = 1;
+        while ((n * (n + 1) / 2) <= S) {
+            n++;
+        }
+
+        System.out.println(n - 1);
+    }
+}
