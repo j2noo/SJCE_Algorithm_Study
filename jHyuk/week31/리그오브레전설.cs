9e2e7ac966a8ba684b863f13641f2a526bc35277
Jaehyuk-Lee <jhlee4938@gmail.com>
2025-03-04 02:43:23 +0900

    이재혁 디렉토리 리뉴얼

--- jHyuk/week31/리그오브레전설.cs
+++ jHyuk/week31/리그오브레전설.cs
@@ -1,8 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program{
-    static void Main(){
-        
+namespace jHyuk.week31
+{
+    class 리그오브레전설
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+        }
     }
 }
