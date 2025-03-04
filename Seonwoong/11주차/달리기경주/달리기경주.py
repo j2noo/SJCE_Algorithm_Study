6b40a1e9edfd1ac545d48911f6755b55e1f943b1
Jinwoo Park <wlsdndml213@naver.com>
2025-03-04 22:08:08 +0900

    Delete Seonwoong directory

--- Seonwoong/11주차/달리기경주/달리기경주.py
+++
@@ -1,9 +0,0 @@
-def solution(players, callings):
-    dic={player: idx for idx, player in enumerate(players)}
-    
-    for name in callings:
-        index=dic[name]
-        players[index], players[index-1]=players[index-1], players[index]
-        dic[players[index]], dic[players[index-1]]=index,index-1
-    
-    return players
--- Seonwoong/20주차/#1. 점찍기.py
+++
@@ -1,7 +0,0 @@
-def solution(k, d):
-    answer = 0 
-    for x in range(0,d+1,k):
-        y=int((d*d-x*x)**(1/2))
-        answer+=y//k+1
-
-    return answer
