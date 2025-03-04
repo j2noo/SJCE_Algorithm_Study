f09b982cede13eb45c52f2581ee8191ea3611d9b
Jinwoo Park <wlsdndml213@naver.com>
2025-03-04 22:08:17 +0900

    Delete geontae directory

--- geontae/29주차/16022-VoronoiVilages.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
-res = []
-arr.sort()
-
-for i in range(1, n-1):
-    res.append(((arr[i] - arr[i-1]) / 2) + ((arr[i+1] - arr[i]) / 2))
-
-print(min(res))
