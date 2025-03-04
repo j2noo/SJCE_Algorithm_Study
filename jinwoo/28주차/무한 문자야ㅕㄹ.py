911d052b3b011e52af0cdfa06710d3f98cf41b4f
Jinwoo Park <wlsdndml213@naver.com>
2025-03-04 22:08:51 +0900

    Delete jinwoo directory

--- jinwoo/28주차/무한 문자야ㅕㄹ.py
+++
@@ -1,8 +0,0 @@
-str1 = input()
-str2 = input()
-
-if str1.startswith(str2) or str2.startswith(str1)  :
-    print(1)
-else :
-    print(0)
-    
--- jinwoo/4주차/30.py
+++
@@ -1,8 +0,0 @@
-N = list(map(int,input()))
-N.sort()
-if sum(N)%3 != 0 or N[0]!=0:
-    print(-1)
-    exit(0)
-N.reverse()
-for i in N:
-    print(i,end="")
