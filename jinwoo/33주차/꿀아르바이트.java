7e086af543c074458d2fb567b611cef7d98df8e3
j2noo <wlsdndml213@gmail.com>
2025-03-23 22:32:51 +0900

    [32주차] 꿀아르바이트

---
+++ jinwoo/33주차/꿀아르바이트.java
@@ -0,0 +1,8 @@
+public class main{
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    System.out.println("!" + str);
+  }
+}
